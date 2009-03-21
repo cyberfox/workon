953bf2bd79b3410e887693554489456d2f910b01
Morgan Schweers <cyberfox@gmail.com>
2009-03-21 04:16:45 -0700

    A basic status model for tracking what's being worked on and what's been done.

---
+++ db/migrate/20090321105247_create_statuses.rb
@@ -0,0 +1,17 @@
+class CreateStatuses < ActiveRecord::Migration
+  def self.up
+    create_table :statuses do |t|
+      t.string   :message
+      t.integer  :user_id
+      t.boolean  :done
+      t.datetime :done_at
+
+      t.timestamps
+    end
+    add_index :statuses, [:user_id, :done_at]
+  end
+
+  def self.down
+    drop_table :statuses
+  end
+end
