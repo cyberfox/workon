f23dfab0a11197e908a5111c9186cb0d608a046e
Morgan Schweers <cyberfox@gmail.com>
2009-03-21 04:13:21 -0700

    Add twitter information to users.

--- db/migrate/20090321104621_create_users.rb
+++ db/migrate/20090321104621_create_users.rb
@@ -10,10 +10,11 @@ class CreateUsers < ActiveRecord::Migration
       t.column :updated_at,                :datetime
       t.column :remember_token,            :string, :limit => 40
       t.column :remember_token_expires_at, :datetime
-
-
+      t.column :twitter_name,              :string
+      t.column :twitter_id,                :integer
     end
     add_index :users, :login, :unique => true
+    add_index :users, :twitter_id
   end
 
   def self.down
