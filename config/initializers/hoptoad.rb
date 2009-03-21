293cb8143793e687410f37663567be6fd5e3ad67
Morgan Schweers <cyberfox@gmail.com>
2009-03-21 03:50:09 -0700

    Initial build from template, with asset_packager, hoptoad, auth, RedCloth, Hpricot, will_paginate, shoulda, quietbacktrace, and capistrano.

---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = 'cee43245752c9be2cf2f015fd78d56ce'
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,2 @@
+  ActionController::Base.session = { :session_key => '_RJQNFN_session', :secret => 'XSEWKLMXPFDRPMBPTZSZWXQAHPVIOEOIKLIDTZGE' }
+  ActionController::Base.session_store = :active_record_store
