f7d56290723309287fd5fcc15640a807dea3748f
Morgan Schweers <cyberfox@gmail.com>
2009-03-21 05:42:34 -0700

    Forgot to remove the quietbacktrace gem from the test environment.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,5 +27,4 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 
-config.gem 'thoughtbot-quietbacktrace', :lib => 'quietbacktrace', :source => 'http://gems.github.com'
 config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com'
