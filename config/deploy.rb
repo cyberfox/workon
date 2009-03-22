212d10410e5baa201227bcaee0ac615a11986395
Morgan Schweers <cyberfox@gmail.com>
2009-03-21 23:49:25 -0700

    Add a capistrano deployment script.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,28 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "workon"
+set :repository,  "git@github.com:cyberfox/workon.git"
+set :scm, "git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/#{application}/app"
+set :user, "workon"
+set :mongrel_config, "/etc/mongrel_cluster/#{application}.yml"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+role :app, "workon.cyberfox.com"
+role :web, "workon.cyberfox.com"
+role :db,  "workon.cyberfox.com", :primary => true
+
+namespace :deploy do
+  task :restart do
+    run "cp ~/config/database.yml.production #{release_path}/config/database.yml"
+    run "mongrel_rails cluster::restart -C #{mongrel_config}"
+  end
+end
